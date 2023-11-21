fe73b0c9e1d03475d0f89a3a6a1f09bdbe27d6d1
SeoHyeonJeong <95207365+202110875@users.noreply.github.com>
2023-11-21 14:01:28 +0900

    Update check_list.dart

--- Self_Diagnosis/check_list.dart
+++ Self_Diagnosis/check_list.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:main/screens/main_screen.dart'; //main.dart
+import 'package:main/screens/main_screen.dart'; //메인 페이지
 import 'package:main/screens/SelfDiagnosis/check_result_screen.dart' as result; //자가진단 결과 페이지 
 
 enum Answer { yes, no }
 
-// 체크 리스트 위젯
+// 자가진단 체크 현황 리스트
 class CheckList extends StatefulWidget {
   final Map<int, Answer?> answerVals; 
   final List<String> Quesets;
-  //const CheckList({Key? key}) : super(key: key);
 
   const CheckList({required this.answerVals, required this.Quesets});
 
@@ -24,55 +23,61 @@ class _CheckListState extends State<CheckList>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white, useMaterial3: true,),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 0,
           //scrolledUnderElevation: 0,
           leading: IconButton(icon: Icon(Icons.arrow_back_ios, size: 30, color: Color.fromRGBO(151, 151, 151, 1)), onPressed:() {
-            Navigator.pop(
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-                builder: (context) => IntroScreen(), // 홈 화면으로 이동
+                builder: (context) => IntroScreen(), // 메인 화면으로 이동
               ),
             );
           }),
         ),
         extendBodyBehindAppBar: true,
-        body: Column(children: [   
-            Container(height: 100,),
+        body: Center(
+          child: Column(children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.13,),
+
             Container(
               width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.all(0),
-              //color:Colors.yellow,
+              height: MediaQuery.of(context).size.height * 0.15,
               child: Text("Self Diagnosis\nComplete !", 
               style: TextStyle(
-                    fontSize: 25,
+                    fontSize: 27,
                     fontFamily: 'Inter',
                     fontWeight: FontWeight.w700,
                     color: Colors.black,
                 ),textAlign: TextAlign.center)
             ),
           
-            // 체크 리스트
-            ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: widget.answerVals.length,
-              itemBuilder: (context, index){
-                final answer = widget.answerVals[index];
-                final isYes = (answer == Answer.yes);
-                return ListTile(
-                  leading: isYes ? Icon(Icons.circle, color: Color.fromRGBO(98, 183, 183, 1), size: 30,) : Icon(Icons.circle_outlined, color: Colors.grey, size: 30,),
-                  title: Text("나는 " + removeNewlines(widget.Quesets[index]),
-                  style: TextStyle(
-                      fontSize: 11,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                  )),
-                );
-              },
+            // 체크 현황 리스트
+            Container(
+              height: MediaQuery.of(context).size.height * 0.55,
+              child: Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount: widget.answerVals.length,
+                  itemBuilder: (context, index){
+                    final answer = widget.answerVals[index];
+                    final isYes = (answer == Answer.yes);
+                    return ListTile(
+                      leading: isYes ? Icon(Icons.circle, color: Color.fromRGBO(98, 183, 183, 1), size: 30,) : Icon(Icons.circle_outlined, color: Colors.grey, size: 30,),
+                      title: Text("나는 " + removeNewlines(widget.Quesets[index]),
+                      style: TextStyle(
+                          fontSize: 11,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                      )),
+                    );
+                  },
+                ),
+              ),
             ),
 
             // 결과 확인 버튼
@@ -88,9 +93,9 @@ class _CheckListState extends State<CheckList>{
                   ),
                   onPressed: () {
                     if(resultCount(widget.answerVals) >= 4){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => result.YesScreen())); //공황장애 의심 페이지로 이동
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const result.YesScreen())); //Yes 답변이 4개 이상이면, 공황장애 의심 페이지로 이동
                     } else{
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => result.NoScreen())); //공황장애 미의심 페이지로 이동
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const result.NoScreen())); // 공황장애 미의심 페이지로 이동
                     }
                     
                   },
@@ -99,10 +104,10 @@ class _CheckListState extends State<CheckList>{
                 ),
               )
             )
-            
           ],)
         )
-      );
+      )
+    );
   }
   int resultCount(answerVal){
     int yesCount = 0;
