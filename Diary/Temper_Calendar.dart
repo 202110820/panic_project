04cd0797bdd595d4d81cd43cbdcdcce983526255
202110820 <68941249+202110820@users.noreply.github.com>
2023-12-06 14:07:26 +0900

    Update Temper_Calendar.dart

--- Diary/Temper_Calendar.dart
+++ Diary/Temper_Calendar.dart
@@ -56,179 +56,167 @@ class _TemperCalendarState extends State<TemperCalendar> {
             }),
           ),
           extendBodyBehindAppBar: true,
-          body: Center(child: Column(children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.10,),
+          body: Column(
 
-            TableCalendar(
-              firstDay: DateTime.utc(2023, 1, 1),
-              lastDay: DateTime.utc(2050, 12, 31),
-              focusedDay: _focusedDay,
-              calendarFormat: _calendarFormat,
-              daysOfWeekHeight: MediaQuery.of(context).size.height * 0.08, // 날짜 행렬과 요일 행 사이 간격을 조정
-              headerStyle: HeaderStyle(
-                  formatButtonVisible: false,
-                  //title
-                  titleCentered: true,
-                  titleTextFormatter: (date, locale) {
-                    return '${date.year}\n${DateFormat.MMMM(locale).format(date)}';
-                  },
-                  titleTextStyle: TextStyle(fontSize: 24, fontFamily: 'Inter', fontWeight: FontWeight.bold),
-                  // left
-                  leftChevronVisible: true,
-                  //right
-                  rightChevronVisible: true,
-                  //header
-                  headerMargin: EdgeInsets.fromLTRB(0, 30, 0, 20)
-              ),
-              calendarStyle: CalendarStyle(
-                outsideDaysVisible: false,
-                cellMargin:  EdgeInsets.all(0), // 날짜 크기 조절
-                cellAlignment: Alignment.topCenter, // 날짜를 네모칸 기준으로 상단 센터에 위치
-                //cellPadding: EdgeInsets.all(0), // 내부 패딩 제거
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.09,),
+                TableCalendar(
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2050, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            daysOfWeekHeight: MediaQuery.of(context).size.height * 0.08, // 날짜 행렬과 요일 행 사이 간격을 조정
+            headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                //title
+                titleCentered: true,
+                titleTextFormatter: (date, locale) {
+                  return '${date.year}\n${DateFormat.MMMM(locale).format(date)}';
+                },
+                titleTextStyle: TextStyle(fontSize: 24, fontFamily: 'Inter', fontWeight: FontWeight.bold),
+                // left
+                leftChevronVisible: true,
+                //right
+                rightChevronVisible: true,
+                //header
+                headerMargin: EdgeInsets.fromLTRB(0, 30, 0, 20)
+            ),
+            calendarStyle: CalendarStyle(
+              outsideDaysVisible: false,
+              cellMargin:  EdgeInsets.all(0), // 날짜 크기 조절
+              cellAlignment: Alignment.topCenter, // 날짜를 네모칸 기준으로 상단 센터에 위치
+              //cellPadding: EdgeInsets.all(0), // 내부 패딩 제거
 
-                todayDecoration: BoxDecoration(
-                  //color: Color.fromRGBO(162, 196, 201, 0.8),
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.white,width: 1.5)
-                ),
-
-                todayTextStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(98, 183, 183, 1),
-                ),
-
-                selectedDecoration: BoxDecoration(
-                    color: Color.fromRGBO(217, 230, 233, 1), //Color.fromRGBO(154, 199, 206, 0.8),
-                    shape: BoxShape.rectangle,
-                    border: Border.all(color: Colors.white,width: 1.5)
-                ),
-                selectedTextStyle: TextStyle(
-                  //fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                defaultTextStyle: TextStyle(color: Colors.black,),
-                weekendTextStyle: TextStyle(color: Colors.red),
+              todayDecoration: BoxDecoration(
+                //color: Color.fromRGBO(162, 196, 201, 0.8),
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white,width: 1.5)
               ),
 
-              selectedDayPredicate: (day) {
-                // Use `selectedDayPredicate` to determine which day is currently selected.
-                // If this returns true, then `day` will be marked as selected.
+              todayTextStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(98, 183, 183, 1),
+              ),
 
-                // Using `isSameDay` is recommended to disregard
-                // the time-part of compared DateTime objects.
-                return isSameDay(_selectedDay, day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                if (!isSameDay(_selectedDay, selectedDay)) {
-                  // Call `setState()` when updating the selected dayseo
-                  setState(() {
-                    _selectedDay = selectedDay;
-                    _focusedDay = focusedDay;
-                    selectedcheckDay = true;
-                  });
-                }
-              },
-              /*
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                // Call `setState()` when updating calendar format
+              selectedDecoration: BoxDecoration(
+                  color: Color.fromRGBO(217, 230, 233, 1), //Color.fromRGBO(154, 199, 206, 0.8),
+                  shape: BoxShape.rectangle,
+                  border: Border.all(color: Colors.white,width: 1.5)
+              ),
+              selectedTextStyle: TextStyle(
+                //fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              defaultTextStyle: TextStyle(color: Colors.black,),
+              weekendTextStyle: TextStyle(color: Colors.red),
+            ),
+
+            selectedDayPredicate: (day) {
+              // Use `selectedDayPredicate` to determine which day is currently selected.
+              // If this returns true, then `day` will be marked as selected.
+
+              // Using `isSameDay` is recommended to disregard
+              // the time-part of compared DateTime objects.
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                // Call `setState()` when updating the selected dayseo
                 setState(() {
-                  _calendarFormat = format;
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  selectedcheckDay = true;
                 });
               }
             },
-
-            onPageChanged: (focusedDay) {
-              // No need to call `setState()` here
-              _focusedDay = focusedDay;
-            },
-            */
-
-              // 온도에 따른 하트 이미지
-              calendarBuilders: CalendarBuilders(
-                markerBuilder: (context, date, events) {
-                  // firebase
-                  return StreamBuilder <QuerySnapshot>(
-                      stream: getCalendar(date, widget.email),
-                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-                        // 기록된 날짜가 없으면 - 빈 하트
-                        if (!snapshot.hasData || snapshot.data!.docs.isEmpty){
-                          return Column(
-                            children: [
-                              const SizedBox(height: 20.0),
-                              Center(
-                                  child: Image.asset('assets/Calendar/HeartPercentage/empty_heart.png', width: 24, height: 24)
-                              ),
-                            ],
-                          );
-                        }
-
-                        // 기록된 날짜 있으면
-                        else {
-                          final documents = snapshot.data!.docs;
-                          final widgets = documents.map((DocumentSnapshot document){
-                            return InkWell(
-                              child: Column(
-                                children: [
-                                  const SizedBox(height: 20.0),
-                                  Center(
-                                      child: Image.asset('assets/Calendar/HeartPercentage/heart${document['temperature']}.png', width: 24, height: 24)
-                                  ),
-                                ],
-                              ),
-                              onTap: () {
-                                setState(() {
-                                  checkedDetail = true;
-                                });
-                                if(document['day'] != null){
-                                  if(checkedDetail == true){
-                                    if('assets/Calendar/HeartPercentage/heart${document['temperature']}.png' != 'assets/Calendar/HeartPercentage/empty_heart.png'){
-                                      // 이벤트를 클릭할 때 세부 정보 팝업 열기
-                                      _showDetailsPopup(context, date);
-                                    }
-                                  }
-
-                                }
-                              },
-                            );
-                          }).toList();
-                          return Column(children: widgets,);
-                        }
-                        //return const InkWell();
+            // 온도에 따른 하트 이미지
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, date, events) {
+                // firebase
+                return StreamBuilder <QuerySnapshot>(
+                    stream: getCalendar(date, widget.email),
+                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+                      // 기록된 날짜가 없으면 - 빈 하트
+                      if (!snapshot.hasData || snapshot.data!.docs.isEmpty){
+                        return Column(
+                          children: [
+                            const SizedBox(height: 20.0),
+                            Center(
+                                child: Image.asset('assets/Calendar/HeartPercentage/empty_heart.png', width: 24, height: 24)
+                            ),
+                          ],
+                        );
                       }
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.10,),
 
-            // 다이어리 추가 버튼
-            Container(
-                alignment: Alignment.center,
-                height: MediaQuery.of(context).size.height * 0.08,
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(98, 183, 183, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  onPressed: () async {
-                    if(selectedcheckDay == true){
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DiaryExample(selectedDay: _selectedDay, email: widget.email), //온도 및 일기 작성 페이지로 이동
-                        ),
-                      );
+                      // 기록된 날짜 있으면
+                      else {
+                        final documents = snapshot.data!.docs;
+                        final widgets = documents.map((DocumentSnapshot document){
+                          return InkWell(
+                            child: Column(
+                              children: [
+                                const SizedBox(height: 20.0),
+                                Center(
+                                    child: Image.asset('assets/Calendar/HeartPercentage/heart${document['temperature']}.png', width: 24, height: 24)
+                                ),
+                              ],
+                            ),
+                            onTap: () {
+                              setState(() {
+                                checkedDetail = true;
+                              });
+                              if(document['day'] != null){
+                                if(checkedDetail == true){
+                                  if('assets/Calendar/HeartPercentage/heart${document['temperature']}.png' != 'assets/Calendar/HeartPercentage/empty_heart.png'){
+                                    // 이벤트를 클릭할 때 세부 정보 팝업 열기
+                                    _showDetailsPopup(context, date);
+                                  }
+                                }
+
+                              }
+                            },
+                          );
+                        }).toList();
+                        return Column(children: widgets,);
+                      }
+                      //return const InkWell();
                     }
-                  },
-                  child: Icon(Icons.add_outlined, size: 50, color: Colors.white),
-                )
+                );
+              },
             ),
-          ]),)
-      ),
+          ),
+                // 다이어리 추가 버튼
+                Expanded(
+                  child: Container(
+                      margin: EdgeInsets.only(bottom: 50),
+                      alignment: Alignment.bottomCenter,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if(selectedcheckDay == true){
+                            await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DiaryExample(selectedDay: _selectedDay, email: widget.email), //온도 및 일기 작성 페이지로 이동
+                              ),
+                            );
+                          }
+                        },
+                        child: Icon( //<-- SEE HERE
+                          Icons.add,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(), //<-- SEE HERE
+                            padding: const EdgeInsets.fromLTRB(0,0,0,0),
+                          backgroundColor: Color(0xFF62B6B6)
+                        ),
+                      ),
+                  ),
+                ),
+            ],
+          )
+      )
     );
   }
 
