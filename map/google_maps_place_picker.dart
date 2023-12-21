910288e55511cb137eaccfbc3bbea0de31f4d35d
202110820 <68941249+202110820@users.noreply.github.com>
2023-12-21 15:08:04 +0900

    Delete map directory

--- map/google_maps_place_picker.dart
+++
@@ -1,6 +0,0 @@
-library google_maps_place_picker;
-
-export 'pick_result.dart';
-export 'floating_card.dart';
-export 'rounded_frame.dart';
-export 'place_picker.dart';
--- map/keys.dart
+++
@@ -1,3 +0,0 @@
-class APIKeys {
-  static String apiKey = 'AIzaSyC2kycO-3FBE0dNBhVbDrdHa3XYMUV7EXI';
-}
